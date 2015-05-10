2285487c85a9e5087566573420acce4ff11b9bdd
Richard Fortune <richard.fortune@xero.com>
2015-05-10 22:14:29 +1200

    created basic page setup plus partials. Submit fails due to 'InvalidAuthenticityToken in HomeController#temp' Error

--- HU/app/controllers/home_controller.rb
+++ HU/app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def about
+  end
+
+   def temp
+   	skip_before_filter  :verify_authenticity_token
+   	redirect_to root_path
+  end
 end
